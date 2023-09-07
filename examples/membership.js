e9cefec47297f2c388249ae9693e30cfa01d2968
Lewis Cheung <lewislunsj@gmail.com>
2023-09-07 10:55:07 +0800

    feat: context cancel

--- examples/membership.js
+++ examples/membership.js
@@ -1,7 +1,7 @@
 import { TelegramCliBot } from '../index.js'
 
 const TOKEN = process.env.TELEGRAM_BOT_TOKEN
-const bot = new TelegramCliBot(TOKEN)
+const bot = new TelegramCliBot(TOKEN, { logger: console })
 const members = [{ name: 'John', age: 20 }]
 
 bot.addCommand({
