f2e232e671f8e8abf58d87155ffa257a05dfce12
Lewis Cheung <lewislunsj@gmail.com>
2023-09-18 22:03:48 +0800

    feat: handle callback query

--- examples/math-bot.js
+++ examples/math-bot.js
@@ -33,7 +33,7 @@ bot.addCommand({
 			one_time_keyboard: true,
 			keyboard: [
 				['-3.14', '4 + 3i', '42'],
-				['2σ', '666', 'F = ma'],
+				['dy/dx', '666', 'F = ma'],
 			]
 		}})
 		const msg = await ctx.waitForMessage()  // button presses will be sent as text messages
@@ -41,24 +41,29 @@ bot.addCommand({
 	}
 })
 
-// bot.addCommand({
-// 	name: 'math3',
-// 	description: 'Math question with Inline Keyboard Markup',
-// 	handler: async ctx => {
-// 		const question = escapeMarkdownV2('What is 30 + 12?')
-// 		await ctx.reply(question, { reply_markup: {
-// 			one_time_keyboard: true,
-// 			inline_keyboard: [
-// 				[ { text: '-3.14', callback_data: '-3.14' }, { text: '4 + 3i', callback_data: '4 + 3i' }, { text: '42', callback_data: '42' } ],
-// 				[ { text: '2σ', callback_data: '2σ' }, { text: '666', callback_data: '666' }, { text: 'F = ma', callback_data: 'F = ma' } ],
-// 			]
-// 		}})
-// 		// bot.edit
-// 		// bot.on('callback_query', async query => {
-// 		// 	console.log('callback_query', query)
-// 		// })
-// 	}
-// })
+bot.addCommand({
+	name: 'math3',
+	description: 'Math question with Inline Keyboard Markup',
+	handler: async ctx => {
+		const question = escapeMarkdownV2('What is 30 + 12?')
+		const inlineKeyboardMsg = await ctx.reply(question, { reply_markup: {
+			inline_keyboard: [
+				[
+					{ text: '-3.14', callback_data: '-3.14' },
+					{ text: '4 + 3i', callback_data: '4 + 3i' },
+					{ text: '42', callback_data: '42' },
+				],
+				[
+					{ text: 'dy/dx', callback_data: 'dy/dx' },
+					{ text: '666', callback_data: '666' },
+					{ text: 'F = ma', callback_data: 'F = ma' },
+				],
+			]
+		}})
+		const query = await ctx.waitForCallbackQueryOnce(inlineKeyboardMsg)
+		await ctx.reply(query.data === '42'? 'Correct' : 'Wrong')
+	}
+})
 
 
 // By calling syncCommands(), /beep will be shown in the command list in the chat with the bot
